5a45cb49ca84863aa430bb37608c8970dd2df937
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-01-17 22:31:08 +0900

    wip

---
+++ apg4b/113.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  ifstream in("input.txt");
+  cin.rdbuf(in.rdbuf());
+
+  int N;
+  cin >> N;
+
+  vector<int> a(N);
+  for (int i = 0; i < N; i++) {
+    cin >> a.at(i);
+  }
+
+  int ave = accumulate(a.begin(), a.end(), 0.0) / a.size();
+  for (int i = 0; i < N; i++) {
+    cout << abs(a.at(i) - ave) << endl;
+  }
+}
