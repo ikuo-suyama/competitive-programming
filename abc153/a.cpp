8100019971eb731acdeb51b537e5d16a887abf84
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-01-25 21:41:13 +0900

    initialize for abc153

---
+++ abc153/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+typedef long long ll;
+
+int main() {
+  ifstream in("abc153/a.txt");
+  cin.rdbuf(in.rdbuf());
+
+  int N;
+  cin >> N;
+
+  int c[N];
+  rep(i, N) { cin >> c[i]; }
+
+  ll cnt = 0;
+
+  cout << cnt << endl;
+}
