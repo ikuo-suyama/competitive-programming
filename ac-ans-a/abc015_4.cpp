4c052f0a4a808e2fec580a2f10d6bf2b6f836643
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-01-28 23:16:38 +0900

    dp solved

--- ac-ans-a/abc015_4.cpp
+++ ac-ans-a/abc015_4.cpp
@@ -8,6 +8,13 @@ typedef long long ll;
  * 動的計画法/DP Dynamic Programming
  * https://qiita.com/drken/items/a5e6fe22863b7992efdb
  */
+/* dp[i+1][j+1][k]= i番目以下のスクショからj個以下選び、幅kがWを超えない
+   スクショを選んだ時の重要度のmax
+   i番目を取らないとき
+   dp[i+1][j+1][k] = max(dp[i][j+1][k],dp[i][j][k])
+   i番目を取る時
+   dp[i+1][j+1][k] = max(dp[i][j+1][k],dp[i][j][k],dp[i][j][k-A[i]] + B[i])
+*/
 int main() {
   ifstream in("ac-ans-a/abc015_4.txt");
   cin.rdbuf(in.rdbuf());
@@ -20,19 +27,25 @@ int main() {
   rep(i, N) { cin >> A[i] >> B[i]; }
 
   vector<vector<vector<int>>> dp(
-      N + 1, vector<vector<int>>(W + 1, vector<int>(K + 2, 0)));
-  for (int i = 1; i < N; i++) {
-    for (int w = 1; w <= W; w++) {
-      for (int k = 1; k <= K; k++) {
+      N + 10, vector<vector<int>>(K + 10, vector<int>(W + 10, 0)));
+  for (int i = 0; i < N; i++) {
+    for (int k = 0; k < K; k++) {
+      for (int w = 0; w <= W; w++) {
+        // printf(
+        //     "i: %d, k:%d, w:%d | dp[i][k][w - A[i]] + B[i]: %d, dp[i][k + "
+        //     "1][w]: %d, dp[i][k][w]d: %d\n",
+        //     i, k, w, dp[i][k][w - A[i]] + B[i], dp[i][k + 1][w],
+        //     dp[i][k][w]);
+
         if (w >= A[i]) {
-          dp[i + 1][w][k] =
-              max({dp[i][w - A[i]][k] + B[i], dp[i][w][k - 1], dp[i][w][k]});
+          dp[i + 1][k + 1][w] =
+              max({dp[i][k][w - A[i]] + B[i], dp[i][k + 1][w], dp[i][k][w]});
         } else {
-          dp[i + 1][w][k] = max(dp[i][w][k], dp[i][w][k - 1]);
+          dp[i + 1][k + 1][w] = max(dp[i][k + 1][w], dp[i][k][w]);
         }
       }
     }
   }
 
-  cout << dp[N][W][K] << endl;
+  cout << dp[N][K][W] << endl;
 }
