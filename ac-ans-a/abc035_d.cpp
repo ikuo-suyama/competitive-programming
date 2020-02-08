c62e62731c6e3f369fcabbfaf514d4b3bd54b637
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-02-08 23:45:29 +0900

    dijkstra2

--- ac-ans-a/abc035_d.cpp
+++ ac-ans-a/abc035_d.cpp
@@ -5,6 +5,33 @@ using namespace std;
 typedef pair<int, int> P;
 typedef long long ll;
 
+typedef pair<ll, int> pl;
+
+void search_dijkstra(vector<ll>& fixed, vector<vector<int>>& g,
+                     map<P, int>& m) {
+  // 未確定なもののQueue; (かかる時間, 島i)
+  priority_queue<pl, vector<pl>, greater<pl>> que;
+
+  que.push(make_pair(0, 0));
+  while (!que.empty()) {
+    auto top = que.top();
+    que.pop();
+    int from = top.second;
+    if (fixed[from] != -1) continue;
+
+    // 未確定のうち一番小さいものを確定
+    ll now = top.first;
+    fixed[from] = now;
+
+    for (auto to : g[from]) {
+      if (fixed[to] != -1) continue;
+      que.push(make_pair(now + m.at(make_pair(from, to)), to));
+    }
+  }
+
+  return;
+}
+
 /**
  * Dijkstra / ダイクストラ法
  */
@@ -20,41 +47,28 @@ int main() {
   vector<int> v(N);
   rep(i, N) { cin >> v[i]; }
 
+  vector<vector<int>> g(N);
+  vector<vector<int>> rev_g(N);
+  map<P, int> cm, rev_cm;
   // M本の有向グラフ
-  vector<vector<int>> g(M);
-  map<P, int> cm;
   rep(i, M) {
     int a, b, c;
     cin >> a >> b >> c;
     a--;
     b--;
     g[a].push_back(b);
+    rev_g[b].push_back(a);
+
     cm.emplace(make_pair(a, b), c);
+    rev_cm.emplace(make_pair(b, a), c);
   }
 
   // ダイクストラ法：各辺への最短経路を求める
   vector<ll> fixed(N, -1);
-  
-  // auto cmp = [](pair<int, ll> a, pair<int, ll> b) { return a.second < b.second; };
-  // 未確定なもののQueue; (かかる時間, 島i)
-  priority_queue<pair<ll, int>> que;
+  search_dijkstra(fixed, g, cm);
 
-  que.push(make_pair(0, 0));
-  while (!que.empty()) {
-    auto top = que.top();
-    que.pop();
-    int from = top.second;
-    if (fixed[from] != -1) continue;
-
-    // 未確定のうち一番小さいものを確定
-    ll now = top.first;
-    fixed[from] = now;
-
-    for (auto to : g[from]) {
-      if (fixed[to] != -1) continue;
-      que.push(make_pair(now + cm.at(make_pair(from, to)), to));
-    }
-  }
+  vector<ll> rev_fixed(N, -1);
+  search_dijkstra(rev_fixed, rev_g, rev_cm);
 
   ll cnt = 0;
   cout << cnt << endl;
