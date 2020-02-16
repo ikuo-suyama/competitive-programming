3792e24fcf4994db4877069edffc330d28ad76e4
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-02-16 20:59:18 +0900

    abc155 init

---
+++ abc155/abc155_b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+typedef pair<int, int> P;
+typedef long long ll;
+
+int main() {
+  ifstream in("abc155/abc155_b.txt");
+  cin.rdbuf(in.rdbuf());
+
+  int N;
+  cin >> N;
+  
+  vector<int> c(N);
+  rep(i, N) { cin >> c[i]; }
+
+  ll cnt = 0;
+
+  cout << cnt << endl;
+}
