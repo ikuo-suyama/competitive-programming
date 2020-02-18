b84e7d1a48b05fc973cbb9712023076c3eeef264
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-02-18 21:42:25 +0900

    abc155 rest

--- abc155/abc155_d.cpp
+++ abc155/abc155_d.cpp
@@ -4,31 +4,63 @@ using namespace std;
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 typedef pair<int, int> P;
 typedef long long ll;
+const int INF = 100100100;
+const ll LL_INF = 1e18 + 1;
+const int MOD = 1e9 + 7;
 
 int main() {
   ifstream in("abc155/abc155_d.txt");
   cin.rdbuf(in.rdbuf());
 
-  int N, K;
+  ll N, K;
   cin >> N >> K;
 
-  vector<ll> p, n;
-  vector<ll> c(N, 0);
-  rep(i, N) {
-    ll a;
-    cin >> a;
-    if (a < 0) {
-      n.push_back(a);
-    } else {
-      p.push_back(a);
+  vector<ll> c(N);
+  rep(i, N) { cin >> c[i]; }
+  sort(c.begin(), c.end());
+
+  ll l = -LL_INF;
+  ll r = LL_INF;
+  while (l + 1 < r) {
+    // k個のときのペアの積の最大値を二分探索する
+    ll x = (l + r) / 2;
+    ll total = 0;
+
+    rep(i, N) {
+      // c[i] と組にしてXを超えないペアの数を二分探索で求める
+      int ll = i, rr = N;
+      if (c[i] > 0) {
+        while (ll + 1 < rr) {
+          int xx = (ll + rr) / 2;
+          if (c[i] * c[xx] < x) {
+            ll = xx;
+          } else {
+            rr = xx;
+          }
+        }
+        total += ll - i;
+        // printf("x: %d, c[i]:%lld ll:%d, total:%d\n", x, c[i], ll, total);
+      } else {
+        // c[i] が負の場合は、後ろから小さい数になる
+        while (ll + 1 < rr) {
+          int xx = (ll + rr) / 2;
+          if (c[i] * c[xx] < x) {
+            rr = xx;
+          } else {
+            ll = xx;
+          }
+        }
+        total += N - rr;
+        // printf("x: %d, c[i]:%lld ll:%d, total:%d\n", x, c[i], ll, total);
+      }
     }
-    c[i] = a;
+
+    if (total < K) {
+      l = x;
+    } else {
+      r = x;
+    }
   }
 
-  vector<ll> ans;
-  rep(i, N) for (int j = i + 1; j < N; j++) { ans.push_back(c[i] * c[j]); }
-
-  sort(ans.begin(), ans.end());
-
-  cout << ans[K -1] << endl;
+  cout << l << endl;
 }
