5c41ad000afd89aaa5727f0cf841a6312836bb30
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-02-29 22:58:43 +0900

    150

---
+++ abc150/abc150_a.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                    \
+  ifstream in("abc157/abc157_a.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE;
+  CIN_OPTIMIZE;
+
+  int K, X;
+  cin >> K >> X;
+
+  string ans;
+  if (K * 500 >= X) {
+    ans = "Yes";
+  } else {
+    ans = "No";
+  }
+
+  cout << ans << endl;
+}
