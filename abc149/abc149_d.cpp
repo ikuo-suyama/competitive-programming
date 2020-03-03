cc74d1b6795de354d99dc121bf50b9c9a1910de7
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-03-03 22:44:03 +0900

    dp wip

--- abc149/abc149_d.cpp
+++ abc149/abc149_d.cpp
@@ -17,11 +17,29 @@ typedef long long ll;
 const int INF = 100100100;
 const int MOD = 1e9 + 7;
 
+// i回目のじゃんけん、r/s/p
+ll dp[10010][3];
+int N, K;
+
+ll memo(int i, int k) {
+  if (i > N) return 0;
+  if (dp[i][k] != -1) return dp[i][k];
+
+  ll ret = 0;
+  rep(l, 3) { ret = memo(i + 1, l); }
+  return dp[i][k] = ret;
+}
+
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
-  int N, K, R, S, P;
+  int R, S, P;
   cin >> N >> K >> R >> S >> P;
+  map<char, int> win;
+  win.emplace('r', 0);
+  win.emplace('s', 1);
+  win.emplace('p', 2);
+
   map<char, int> points;
   // 相手の手に対して勝利したときの点
   points.emplace('s', R);
