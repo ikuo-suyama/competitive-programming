f98f4f457d3ecbfc0f33246fb566be6450036305
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-09 13:23:45 +0900

    ng

---
+++ abc100/abc100_b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("abc100/abc100_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int d, n;
+  cin >> d >> n;
+
+  d = pow(100, d);
+  ll ans = d * n;
+  rep(i, n) { 
+    ans += d;
+  }
+
+  cout << ans << endl;
+}
