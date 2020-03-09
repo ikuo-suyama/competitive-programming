1ecff392d4b84a9bfc58b62fa7694a63a19301aa
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-09 14:15:27 +0900

    ok;

--- abc100/abc100_b.cpp
+++ abc100/abc100_b.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                   \
+#define INPUT_FILE                    \
   ifstream in("abc100/abc100_b.txt"); \
   cin.rdbuf(in.rdbuf());
 #else
@@ -15,7 +15,7 @@ using namespace std;
 typedef pair<int, int> P;
 typedef long long ll;
 const int INF = 100100100;
-const ll LINF = 1e18+100;
+const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
 int main() {
@@ -26,7 +26,7 @@ int main() {
 
   d = pow(100, d);
   ll ans = d * n;
-  rep(i, n) { 
+  if (n == 100) {
     ans += d;
   }
 
