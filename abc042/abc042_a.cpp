4d12ed53277a93f209a4da798ca184c0d60bb42e
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-10 14:30:55 +0900

    042

---
+++ abc042/abc042_a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                    \
+  ifstream in("abc042/abc042_a.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  vector<int> c(3);
+  rep(i, 3) { cin >> c[i]; }
+  sort(c.begin(), c.end());
+  string ans = "NO";
+  if (c[0] == 5 && c[1] == 5 && c[2] == 7) ans = "YES";
+
+  cout << ans << endl;
+}
