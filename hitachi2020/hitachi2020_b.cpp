8746aa8ebb24f703175caf1b2d517c5fa301fe3d
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-13 11:00:30 +0900

    a

---
+++ hitachi2020/hitachi2020_b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("hitachi2020/hitachi2020_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int A, B, M;
+  cin >> A >> B >> M;
+  
+  vector<int> a(A);
+  vector<int> b(B);
+  vector<int> c(M);
+  rep(A)
+
+  ll cnt = 0;
+
+  cout << cnt << endl;
+}
