c9f773c9df072ce675b9fb54381c98fea6e894a0
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-13 22:17:31 +0900

    hitachi

--- hitachi2020/hitachi2020_b.cpp
+++ hitachi2020/hitachi2020_b.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                   \
+#define INPUT_FILE                              \
   ifstream in("hitachi2020/hitachi2020_b.txt"); \
   cin.rdbuf(in.rdbuf());
 #else
@@ -15,7 +15,7 @@ using namespace std;
 typedef pair<int, int> P;
 typedef long long ll;
 const int INF = 100100100;
-const ll LINF = 1e18+100;
+const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
 int main() {
@@ -23,13 +23,26 @@ int main() {
 
   int A, B, M;
   cin >> A >> B >> M;
-  
-  vector<int> a(A);
-  vector<int> b(B);
-  vector<int> c(M);
-  rep(A)
 
-  ll cnt = 0;
+  vector<ll> a(A);
+  vector<ll> b(B);
 
-  cout << cnt << endl;
+  rep(i, A) { cin >> a[i]; }
+  rep(i, B) { cin >> b[i]; }
+
+  ll ans = LINF;
+  rep(i, M) {
+    ll x, y, c;
+    cin >> x >> y >> c;
+    x--;
+    y--;
+    ans = min(ans, a[x] + b[y] - c);
+  }
+
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  ans = min(ans, a[0] + b[0]);
+
+  cout << ans << endl;
 }
