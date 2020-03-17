6f5ba02721dfd8b08c0d1a51aed6ca5b23a6a2b3
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-17 09:00:23 +0900

    problems

--- atcoder-problems/abc130_c.cpp
+++ atcoder-problems/abc130_c.cpp
@@ -18,28 +18,14 @@ const int INF = 100100100;
 const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
-using ld = long double;
-constexpr ld EPS = 1E-9;
-// r の(誤差付きの)符号に従って, -1, 0, 1 を返す.
-int sgn(const ld& r) { return (r > EPS) - (r < -EPS); }
-// a, b の(誤差付きの)大小比較の結果に従って, -1, 0, 1 を返す.
-int sgn(const ld& a, const ld& b) { return sgn(a - b); }
-
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
   ll W, H;
   cin >> W >> H;
 
-  long double x, y;
+  ll x, y;
   cin >> x >> y;
 
-  // x
-  long double minx = sgn(x * H, (W - x) * H) < 0 ? x * H : (W - x) * H;
-  // y
-  long double miny = sgn(y * W, (H - y) * W) < 0 ? y * W : (H - y) * W;
-
-  long double ans = sgn(minx, miny) > 0 ? minx : miny;
-  int check = sgn(minx, miny) == 0 ? 1 : 0;
-  printf("%Lf %d", ans, check);
+  printf("%Lf %d", (long double)W * H / 2, 2 * x == W && 2 * y == H);
 }
