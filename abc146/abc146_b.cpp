8a84108337c282adda05f7106f05b3aeae06c120
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-03-20 22:30:59 +0900

    146 c

---
+++ abc146/abc146_b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("abc146/abc146_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+
+  string ans = "";
+  rep(i, S.size()) {
+    ans += S[i] + N;
+    if (ans[i] > 'Z') ans[i] -= 26;
+  }
+
+  cout << ans << endl;
+}
