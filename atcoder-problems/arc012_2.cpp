04dd70b9e028a6fc6786dab83f1be20cbf97c830
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-03-21 22:30:28 +0900

    problems

---
+++ atcoder-problems/arc012_2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("atcoder-problems/arc012_2.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int N, va, vb, L;
+  cin >> N >> va >> vb >> L;
+
+  long double l = L;
+  rep(i, N) { 
+    long double s = l / va;
+    l = s * vb;
+  }
+
+  printf("%6Lf", l);
+}
