9d4d5503f7cacdb54e0fd38f8a5182257138d821
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-04-10 15:28:10 +0900

    daily work

---
+++ atcoder-problems/abc044_b.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                              \
+  ifstream in("atcoder-problems/abc044_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  string S;
+  cin >> S;
+
+  map<char, int> m;
+  rep(i, S.size()) {
+    m[S[i]] += 1;
+  }
+
+  bool ok = true;
+  for (auto kv : m) {
+    ok &= kv.second % 2 == 0;
+  }
+
+  cout << (ok ? "Yes" : "No") << endl;
+}
