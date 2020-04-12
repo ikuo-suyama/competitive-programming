7950428fb39a48a2276893f183b72ddb156a5cc1
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-04-12 16:18:45 +0900

    ltest;

---
+++ language-test-202001/abc086_a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                                  \
+  ifstream in("language-test-202001/abc086_a.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int a, b;
+  cin >> a >> b;
+
+  bool even = a % 2 == 0 || b % 2 == 0;
+
+  cout << (even ? "Even" : "Odd") << endl;
+}
