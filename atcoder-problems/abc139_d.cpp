46c3d7043bd682ff6d6dee2c34258120933a4bcd
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-04-16 10:12:15 +0900

    daily work

---
+++ atcoder-problems/abc139_d.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                              \
+  ifstream in("atcoder-problems/abc139_d.txt"); \
+  cin.rdbuf(in.rdbuf());
+#else
+#define INPUT_FILE
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  ll N;
+  cin >> N;
+  cout << ((N - 1) * N) / 2 << endl;
+}
