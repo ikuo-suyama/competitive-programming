05351509041b07d0c93cec554bcdfc5dc1438ca7
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-04-20 21:35:12 +0900

    daily

--- atcoder-problems/abc074_b.cpp
+++ atcoder-problems/abc074_b.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                    \
-  ifstream in("abc163/abc163_d.txt"); \
+#define INPUT_FILE                   \
+  ifstream in("atcoder-problems/abc074_b.txt"); \
   cin.rdbuf(in.rdbuf());
 #else
 #define INPUT_FILE
@@ -15,7 +15,7 @@ using namespace std;
 typedef pair<int, int> P;
 typedef long long ll;
 const int INF = 100100100;
-const ll LINF = 1e18 + 100;
+const ll LINF = 1e18+100;
 const int MOD = 1e9 + 7;
 
 int main() {
@@ -23,20 +23,12 @@ int main() {
 
   int N, K;
   cin >> N >> K;
-
-  vector<ll> com(N + 1, 0);
-  repi(i, 1, N + 1) { com[i] = com[i - 1] + i; }
+  
+  vector<int> c(N);
+  rep(i, N) { cin >> c[i]; }
 
   ll ans = 0;
-  repi(k, K, N + 2) {
-    ll l = com[k - 1];
-    int ri = N - k >= 0 ? N - k : 0;
-    ll r = com[N] - com[ri];
-
-    ans += (r - l + 1) % MOD;
-    // printf("%d %d: %lld %lld %lld\n", N, k, l, r, ans);
-    ans %= MOD;
-  }
+  rep(i, N) { ans += min(c[i], K - c[i]) * 2; }
 
   cout << ans << endl;
 }
