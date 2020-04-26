d3d65375fc406f88aa31170f91744ffbfc9e04b7
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-04-26 22:32:26 +0900

    d ?

--- abc164/abc164_d.cpp
+++ abc164/abc164_d.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                   \
+#define INPUT_FILE                    \
   ifstream in("abc164/abc164_d.txt"); \
   cin.rdbuf(in.rdbuf());
 #else
@@ -15,19 +15,27 @@ using namespace std;
 typedef pair<int, int> P;
 typedef long long ll;
 const int INF = 100100100;
-const ll LINF = 1e18+100;
+const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
-  int N;
-  cin >> N;
-  
-  vector<int> c(N);
-  rep(i, N) { cin >> c[i]; }
+  string S;
+  cin >> S;
 
-  ll cnt = 0;
+  ll ans = 0;
 
-  cout << cnt << endl;
+  rep(i, S.size()) {
+    repi(j, 5, 11) {
+      if (i + j > S.size()) break;
+      string s = S.substr(i, j);
+      ll ls = stol(s);
+      if (ls % 2019 == 0) {
+        ans++;
+      }
+    }
+  }
+
+  cout << ans << endl;
 }
