588540878f7f51d0feef1b13b99b43d9d8068138
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-04-30 23:09:31 +0900

    comment

--- atcoder-problems/abc148_e.cpp
+++ atcoder-problems/abc148_e.cpp
@@ -18,6 +18,13 @@ const int INF = 100100100;
 const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
+/**
+ * 整数
+ * あるNのうち、Pで割り切れるものの数は N / P
+ * 100 の中に 5 で割り切れる数は 100 / 5 個ある
+ * 末尾に0をつける -> 5の倍数の数 と 2の倍数の組み合わせ
+ * #integer
+ */
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
