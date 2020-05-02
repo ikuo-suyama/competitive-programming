bf58edd9c641a707477ac11910056c24050f8769
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-05-02 22:32:48 +0900

    mis

--- abc165/abc165_c.cpp
+++ abc165/abc165_c.cpp
@@ -25,46 +25,35 @@ int main() {
   int N, M, Q;
   cin >> N >> M >> Q;
 
-  vector<int> a(Q);
-  vector<int> b(Q);
-  vector<int> c(Q);
-  vector<int> d(Q);
+  vector<tuple<int, int, int, int>> dabc;
   rep(i, Q) {
-    int _a, _b;
-    cin >> _a >> _b >> c[i] >> d[i];
-    _a--;
-    _b--;
-    a[i] = _a;
-    b[i] = _b;
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    a--;
+    b--;
+    dabc.push_back(make_tuple(d, a, b, c));
   }
+  sort(dabc.begin(), dabc.end(), greater<>());
 
   ll ans = 0;
-  rep(i, pow(M, N) + 1) {
-    vector<int> x(N, 1);
-    int tmp = i;
+  vector<int> x(N, M);
+  for(auto _t : dabc) {
+    int d = get<0>(_t);
+    int a = get<1>(_t);
+    int b = get<2>(_t);
+    int c = get<3>(_t);
+    int _b = x[b];
+    int _a = _b - c;
     bool ok = true;
-    for (int j = N - 1; j >= 0; j--) {
-      int p = tmp / pow(M, j);
-      x[j] = p;
-      tmp -= p * pow(M, j);
-      if (j < N - 1 && x[j] < x[j + 1]) {
+    rep(i, a + 1) {
+      if (x[i] != M && x[i] > _a) {
         ok = false;
         break;
+      } else {
+        x[i] = _a;
       }
     }
-    if (!ok) continue;
-    reverse(x.begin(), x.end());
-
-    // printf("i:%d ", i);
-    // rep(l, N) { cout << x[l] << " "; }
-    // cout << endl;
-
-    // 判定
-    ll tmpans = 0;
-    rep(i, Q) {
-      if (x[b[i]] - x[a[i]] == c[i]) tmpans += d[i];
-    }
-    ans = max(tmpans, ans);
+    if(ok) ans += d;
   }
 
   cout << ans << endl;
