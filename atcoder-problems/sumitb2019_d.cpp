ff9fb96aa49059ae7be8e45fd2cf54a87b5c17cd
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-05-09 15:26:11 +0900

    daily work

--- atcoder-problems/sumitb2019_d.cpp
+++ atcoder-problems/sumitb2019_d.cpp
@@ -23,51 +23,40 @@ const int INF = 100100100;
 const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
-ll comb(int n, int k) {
-  if (k == 0) return 1;
-  if (n / 2 < k) return comb(n, n - k);
-  ll ans = 1;
-  vector<int> p(0);
-  vector<int> q(0);
-  rep(i, k) { p.push_back(n - i); }
-  repi(i, 1, k + 1) { q.push_back(i); }
-  rep(i, k) {
-    ans *= p[i];
-    ans /= q[i];
-  }
+int N;
+string S;
+int M = 3;
+vector<int> buf(M, 0);
+int ans = 0;
 
-  return ans;
+void dfs(const int i, const int range_s, const int range_e) {
+  if (i == M) {
+    int j = 0;
+    rep(i, N) {
+      if (S[i] - '0' == buf[j]) {
+        j++;
+      }
+    }
+    if (j >= M) {
+      // print_vec(buf);
+      ans++;
+    }
+    return;
+  }
+  repi(j, range_s, range_e + 1) {
+    buf[i] = j;
+    dfs(i + 1, range_s, range_e);
+  }
 }
 
+/**
+ * #dfs #全探索 #枝刈り
+ */
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
-  int N;
-  string S;
   cin >> N >> S;
 
-  map<char, int> mp;
-  rep(i, N) { mp[S[i]]++; }
-
-  int n = 0, m = 0, l = 0;
-  for (auto kv : mp) {
-    if (kv.second == 1)
-      n++;
-    else if (kv.second == 2)
-      m++;
-    else if (kv.second >= 3)
-      l++;
-  }
-
-  ll ans = 0;
-  // すべてバラバラ
-  ans += comb(n + m + l, 3);
-  // 2つ同じ
-  ans += n * comb(m + l, 2) * 3;
-  ans += m * comb(m + l - 1, 2) * 3;
-  ans += l * comb(m + l - 1, 2) * 3;
-  // 3つ同じ
-  ans += comb(m, 1);
-
+  dfs(0, 0, 9);
   cout << ans << endl;
 }
