0e25b5e0b34c8600cd8220c5b6f7b50e2e82881f
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-05-19 22:21:21 +0900

    daily work

--- atcoder-problems/yahoo_procon2019_qual_c.cpp
+++ atcoder-problems/yahoo_procon2019_qual_c.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                   \
+#define INPUT_FILE                                             \
   ifstream in("atcoder-problems/yahoo_procon2019_qual_c.txt"); \
   cin.rdbuf(in.rdbuf());
-#define print_vec(v) \
-rep(l, v.size()) { cout << v[l] << " "; } \
-cout << endl;
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
 #else
 #define INPUT_FILE
 #define print_vec(v)
@@ -20,7 +20,7 @@ typedef pair<int, int> P;
 typedef long long ll;
 typedef pair<ll, ll> pl;
 const int INF = 100100100;
-const ll LINF = 1e18+100;
+const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
 int main() {
@@ -55,9 +55,11 @@ int main() {
   ll cookie = B;
   k--;
 
-  ll res = k / (A + 2);
-  cookie += res * B;
-  cookie += k % (A + 2);
+  // ll res = k / (A + 2);
+  // cookie += res * B;
+  // cookie += k % (A + 2);
+  cookie += (k / 2) * (B - A);
+  cookie += k % 2;
 
   cout << cookie << endl;
 }
