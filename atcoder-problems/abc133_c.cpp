b450f48a7b6e55f9d738a80265f66ae220969cd7
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-05-24 14:58:13 +0900

    daily work

---
+++ atcoder-problems/abc133_c.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                              \
+  ifstream in("atcoder-problems/abc133_c.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  ll L, R;
+  cin >> L >> R;
+
+  ll ans = INF;
+  repi(i, L, min(R, L + 2019)) {
+    repi(j, L + 1, min(R + 1, L + 2020)) { ans = min(ans, (ll)j * i % 2019); }
+  }
+
+  cout << ans << endl;
+}
