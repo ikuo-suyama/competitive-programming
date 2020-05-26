b334a9ab0c4e26259ce4ac316b0f51b106df557d
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-05-26 13:29:43 +0900

    daily work

---
+++ atcoder-problems/arc101_a.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                              \
+  ifstream in("atcoder-problems/arc101_a.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int N, K;
+ll solve(vector<ll> &c) {
+  vector<int> d(N, 0);
+  rep(i, N - 1) {
+    if (c[i] < 0 && c[i + 1] < 0) {
+      d[i] = (abs(c[i]) - abs(c[i + 1])) * 2;
+    } else if (c[i] < 0) {
+      d[i] = abs(c[i]) * 2 + c[i + 1];
+    } else {
+      d[i] = c[i + 1] - c[i];
+    }
+  }
+
+  ll ans = LINF;
+  ll sum = 0;
+
+  rep(i, N) {
+    if (i < K) {
+      sum += d[i];
+      ans = sum;
+    } else {
+      sum -= d[i - K];
+      sum += d[i];
+      ans = min(ans, sum);
+    }
+  }
+  return ans;
+}
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  cin >> N >> K;
+
+  vector<ll> c(N, 0);
+  rep(i, N) { cin >> c[i]; }
+  if (N == 1) {
+    cout << c[0] << endl;
+    return 0;
+  }
+
+  ll ans = solve(c);
+
+  rep(i, N) { c[i] *= -1; }
+  sort(c.begin(), c.end());
+  ans = min(ans, solve(c));
+
+  cout << ans << endl;
+}
