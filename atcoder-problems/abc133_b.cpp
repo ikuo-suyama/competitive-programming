f35858188b1c18afd21bffa53aa6791c007de2cf
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-06-10 22:56:45 +0900

    daily work

--- atcoder-problems/abc133_b.cpp
+++ atcoder-problems/abc133_b.cpp
@@ -26,13 +26,19 @@ const int MOD = 1e9 + 7;
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
-  int N;
-  cin >> N;
+  int N, D;
+  cin >> N >> D;
   
-  vector<int> c(N);
-  rep(i, N) { cin >> c[i]; }
+  vector<vector<int>> x(N, vector<int>(D, 0));
+  rep(i, N) rep(j, D) { cin >> x[i][j]; }
 
   ll ans = 0;
+  rep(i, N - 1) repi(j, i + 1, N) {
+    int tmp = 0;
+    rep(k, D) { tmp += pow(x[i][k] - x[j][k], 2); }
+    int dis = sqrt(tmp);
+    ans += pow(dis, 2) == tmp;
+  }
 
   cout << ans << endl;
 }
