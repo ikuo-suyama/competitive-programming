589587aaa1ddfa511093618915fa3f09449da57d
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-07-05 20:50:14 +0900

    daily work

---
+++ abc169/abc169_c.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("abc169/abc169_c.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v) \
+rep(l, v.size()) { cout << v[l] << " "; } \
+cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  unsigned long long A;
+  string B;
+  cin >> A >> B;
+
+  unsigned long long C = (B[0] - '0') * 100 + (B[2] - '0') * 10 + (B[3] - '0');
+
+  unsigned long long ans = A * C;
+  ans /= 100;
+
+  cout << ans << endl;
+
+}
