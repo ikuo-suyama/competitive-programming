a6aa6d5f0bbd3e95b9191933dbc88a3d26ac87eb
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-07-13 21:55:01 +0900

    daily work

--- atcoder-problems/abc079_c.cpp
+++ atcoder-problems/abc079_c.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                                     \
-  ifstream in("atcoder-problems/ddcc2020_qual_b.txt"); \
+#define INPUT_FILE                              \
+  ifstream in("atcoder-problems/abc079_c.txt"); \
   cin.rdbuf(in.rdbuf());
 #define print_vec(v)                        \
   rep(l, v.size()) { cout << v[l] << " "; } \
@@ -26,23 +26,26 @@ const int MOD = 1e9 + 7;
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
-  int N;
-  cin >> N;
+  int N = 4;
+  vector<int> c(N, 0);
+  string S;
+  cin >> S;
+  rep(i, N) { c[i] = S[i] - '0'; }
 
-  vector<ll> c(N);
-  ll total = 0;
-  rep(i, N) {
-    cin >> c[i];
-    total += c[i];
+  rep(i, pow(3, 2)) {
+    int ans = c[0];
+    rep(j, 3) {
+      if (i & 1 << j) {
+        ans += c[j + 1];
+      } else {
+        ans -= c[j + 1];
+      }
+    }
+    if (ans == 7) {
+      cout << c[0];
+      rep(j, 3) { cout << ((i & 1 << j) ? "+" : "-") << c[j + 1]; }
+      cout << "=7";
+      return 0;
+    }
   }
-
-  ll left = 0;
-  ll right = total;
-  ll ans = right - left;
-  rep(i, N) {
-    left += c[i];
-    right -= c[i];
-    ans = min(ans, abs(left - right));
-  }
-  cout << ans << endl;
 }
