6f0bf9528c602ff69534c05dd5c56de6d9e28aa8
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-07-15 21:28:45 +0900

    daily work

--- atcoder-problems/abc121_c.cpp
+++ atcoder-problems/abc121_c.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                   \
+#define INPUT_FILE                              \
   ifstream in("atcoder-problems/abc121_c.txt"); \
   cin.rdbuf(in.rdbuf());
-#define print_vec(v) \
-rep(l, v.size()) { cout << v[l] << " "; } \
-cout << endl;
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
 #else
 #define INPUT_FILE
 #define print_vec(v)
@@ -20,19 +20,36 @@ typedef pair<int, int> P;
 typedef long long ll;
 typedef pair<ll, ll> pl;
 const int INF = 100100100;
-const ll LINF = 1e18+100;
+const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
 int main() {
   INPUT_FILE CIN_OPTIMIZE;
 
-  int N;
-  cin >> N;
-  
-  vector<int> c(N);
-  rep(i, N) { cin >> c[i]; }
+  ll N, M;
+  cin >> N >> M;
 
+  vector<pl> c(N);
+  rep(i, N) {
+    ll a, b;
+    cin >> a >> b;
+    c[i] = make_pair(a, b);
+  }
+  sort(c.begin(), c.end());
+
+  ll cnt = 0;
   ll ans = 0;
+  rep(i, N) {
+    ll a = c[i].first;
+    ll b = c[i].second;
+    if (cnt + b < M) {
+      ans += a * b;
+      cnt += b;
+    } else {
+      ans += (M - cnt) * a;
+      break;
+    }
+  }
 
   cout << ans << endl;
 }
