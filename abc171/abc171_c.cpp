dca505db1d2469b37b7ad068d4c9967c0bf34324
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-08-02 22:33:54 +0900

    daily work

---
+++ abc171/abc171_c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                    \
+  ifstream in("abc171/abc171_c.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+/**
+ * 基数変換
+ */
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  ll N;
+  cin >> N;
+
+  string ans = "";
+  while (N > 0) {
+    N--;
+    ans += 'a' + N % 26;
+    N /= 26;
+  }
+  reverse(ans.begin(), ans.end());
+  cout << ans << endl;
+}
