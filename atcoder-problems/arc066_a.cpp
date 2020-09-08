58f5c8b23b63af473a17ef76eb90452d8f9248b2
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-09-08 21:03:55 +0900

    daily work

---
+++ atcoder-problems/arc066_a.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("atcoder-problems/arc066_a.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v) \
+rep(l, v.size()) { cout << v[l] << " "; } \
+cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+// const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int N;
+  cin >> N;
+  
+  unordered_map<int, int> m;
+  rep(i, N) {
+    int c;
+    cin >> c;
+    m[c]++;
+  }
+
+  if (N % 2 == 0) {
+    for (auto kv : m) {
+      if (kv.first % 2 != 1 || kv.second != 2) {
+        cout << 0 << endl;
+        return 0;
+      }
+    }
+  } else {
+    for (auto kv : m) {
+      if (kv.first % 2 != 0 || kv.second != 2) {
+        if (kv.first == 0 && kv.second == 1) continue;
+        cout << 0 << endl;
+        return 0;
+      }
+    }
+  }
+
+  mint ans = 2;
+  cout << ans.pow(N / 2) << endl;
+}
