71adaabaa535707f110beefd53a5778367944757
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-09-14 21:55:36 +0900

    miss

--- atcoder-problems/code_festival_2017_qualc_c.cpp
+++ atcoder-problems/code_festival_2017_qualc_c.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define repi(i, s, n) for (int i = (s); i < (n); i++)
 #ifdef LOCAL
-#define INPUT_FILE                   \
+#define INPUT_FILE                                                \
   ifstream in("atcoder-problems/code_festival_2017_qualc_c.txt"); \
   cin.rdbuf(in.rdbuf());
-#define print_vec(v) \
-rep(l, v.size()) { cout << v[l] << " "; } \
-cout << endl;
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
 #else
 #define INPUT_FILE
 #define print_vec(v)
@@ -20,7 +20,7 @@ typedef pair<int, int> P;
 typedef long long ll;
 typedef pair<ll, ll> pl;
 const int INF = 100100100;
-const ll LINF = 1e18+100;
+const ll LINF = 1e18 + 100;
 const int MOD = 1e9 + 7;
 
 int main() {
@@ -43,9 +43,11 @@ int main() {
   }
 
   ll ans = 0;
-  rep(i, N) {
-    
+  int center = 0;
+  rep(i, ceil((double)c.size() / 2)) {
+    while (S[center] != c[i]) center++;
   }
+
 
   cout << ans << endl;
 }
