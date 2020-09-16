6b15e4593a80b5662fa49c022e8291bbee974552
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-09-16 21:55:22 +0900

    daily work

---
+++ abc178/abc178_d.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("abc178/abc178_d.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v) \
+rep(l, v.size()) { cout << v[l] << " "; } \
+cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+// const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+vector<mint> m(2001, 0);
+mint solve(int n) {
+  if (m[n].x != 0) return m[n];
+
+  int i = 3;
+  mint ret = 0;
+  ret += solve(n - 1);
+  ret += solve(n - 3);
+  return m[n] = ret;
+}
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int N;
+  cin >> N;
+  if (N < 3) {
+    cout << 0 << endl;
+    return 0;
+  }
+
+  m[3] = 1;
+  m[4] = 1;
+  m[5] = 1;
+
+  cout << solve(N) << endl;
+}
