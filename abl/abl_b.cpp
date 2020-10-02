91c1356277011277e599631454fb4be8204852a3
Ikuo Suyama <suyama_ikuo@cyberagent.co.jp>
2020-10-02 09:11:16 +0900

    daily work

---
+++ abl/abl_b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE              \
+  ifstream in("abl/abl_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  ll A, B, C, D;
+  cin >> A >> B >> C >> D;
+
+  bool ok = false;
+  if (A > C) {
+    if (A <= D) {
+      ok = true;
+    }
+  } else {
+    if (C <= B) ok = true;
+  }
+
+  cout << (ok ? "Yes" : "No") << endl;
+}
