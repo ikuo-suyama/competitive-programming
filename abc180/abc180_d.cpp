9db360635fdd5059ce325d1f2b4f61147c97a840
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-10-25 21:55:01 +0900

    daily work

---
+++ abc180/abc180_d.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                    \
+  ifstream in("abc180/abc180_d.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  ll X, Y, A, B;
+  cin >> X >> Y >> A >> B;
+
+  ll ans = 0;
+  // 簡易 オーバーフロー判定
+  while ((double)X * A <= 2e18 && X * A <= X + B && A * X < Y) {
+    X *= A;
+    ans++;
+  }
+
+  cout << ans + (Y - 1 - X) / B << endl;
+}
