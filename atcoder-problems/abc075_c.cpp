1dff90f7474e7fe216865f5f661814b10cdf3c6c
Ikuo Suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-10-29 09:51:09 +0900

    daily work

---
+++ atcoder-problems/abc075_c.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                              \
+  ifstream in("atcoder-problems/abc075_c.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v)                        \
+  rep(l, v.size()) { cout << v[l] << " "; } \
+  cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18 + 100;
+const int MOD = 1e9 + 7;
+
+struct UnionFind {
+  vector<int> par;  // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
+
+  UnionFind(int N) : par(N) {  //最初は全てが根であるとして初期化
+    for (int i = 0; i < N; i++) par[i] = i;
+  }
+
+  int root(int x) {  // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+    if (par[x] == x) return x;
+    return par[x] = root(par[x]);
+  }
+
+  vector<int> cnt;
+  void countUp() {
+    cnt = vector<int>(par.size(), 0);
+    for (int i = 0; i < par.size(); i++) {
+      int r = root(i);
+      cnt[r]++;
+    }
+  }
+  int countConnected(int x) { return cnt[root(x)]; }
+
+  void unite(int x, int y) {  // xとyの木を併合
+    int rx = root(x);         // xの根をrx
+    int ry = root(y);         // yの根をry
+    if (rx == ry) return;  // xとyの根が同じ(=同じ木にある)時はそのまま
+    par[rx] =
+        ry;  // xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+  }
+
+  bool same(int x, int y) {  // 2つのデータx, yが属する木が同じならtrueを返す
+    int rx = root(x);
+    int ry = root(y);
+    return rx == ry;
+  }
+};
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int N, M;
+  cin >> N >> M;
+
+  vector<P> c(M);
+  rep(i, M) {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    c[i] = make_pair(a, b);
+  }
+
+  ll ans = 0;
+  rep(i, M) {
+    UnionFind u(N);
+    rep(j, M) {
+      if (i != j) u.unite(c[j].first, c[j].second);
+    }
+    rep(j, N - 1) { if (!u.same(j, j + 1)) {
+        ans++;
+        break;
+      }
+    }
+  }
+
+  cout << ans << endl;
+}
