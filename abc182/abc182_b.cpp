8b9c968c3c7b37b36c895d85b7bfdbaef29bb041
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-11-10 21:11:35 +0900

    daily work

---
+++ abc182/abc182_b.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("abc182/abc182_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v) \
+rep(l, v.size()) { cout << v[l] << " "; } \
+cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  int N;
+  cin >> N;
+  
+  vector<int> c(N);
+  rep(i, N) { cin >> c[i]; }
+
+  ll ans = 0;
+  int cur = 0;
+  repi(i, 2, 1001) {
+    int k = 0;
+    rep(j, N) {
+      if (c[j] % i == 0) k++;
+    }
+    if (cur <= k) {
+      ans = i;
+      cur = k;
+    }
+  }
+
+  cout << ans << endl;
+}
