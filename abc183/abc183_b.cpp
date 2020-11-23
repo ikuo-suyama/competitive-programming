4ec28494cedcf0d937f3e7e04c583a2758e71cdc
ikuo-suyama <13306175+ikuo-suyama@users.noreply.github.com>
2020-11-23 20:12:44 +0900

    daily work

---
+++ abc183/abc183_b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repi(i, s, n) for (int i = (s); i < (n); i++)
+#ifdef LOCAL
+#define INPUT_FILE                   \
+  ifstream in("abc183/abc183_b.txt"); \
+  cin.rdbuf(in.rdbuf());
+#define print_vec(v) \
+rep(l, v.size()) { cout << v[l] << " "; } \
+cout << endl;
+#else
+#define INPUT_FILE
+#define print_vec(v)
+#endif
+#define CIN_OPTIMIZE \
+  cin.tie(0);        \
+  ios::sync_with_stdio(false);
+typedef pair<int, int> P;
+typedef long long ll;
+typedef pair<ll, ll> pl;
+const int INF = 100100100;
+const ll LINF = 1e18+100;
+const int MOD = 1e9 + 7;
+
+int main() {
+  INPUT_FILE CIN_OPTIMIZE;
+
+  ll X1, Y1, X2, Y2;
+  cin >> X1 >> Y1 >> X2 >> Y2;
+
+  double ans = (double)(Y2 * X1 + Y1 * X2) / (Y1 + Y2);
+
+  printf("%.09lf", ans);
+}
